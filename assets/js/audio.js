44d929f2e1d127d0be97db72213e6be9830f2905
mandarinamp4 <joaquinquiroga77@gmail.com>
2024-10-06 23:56:04 -0300

    Add files via upload

---
+++ assets/js/audio.js
@@ -0,0 +1,150 @@
+const productImage = document.querySelector('.col-lg-4.col-md-6.portfolio-item.filter-construction img'); // Selects the image element
+const productAudio = document.getElementById('productAudio'); // Selects the audio element with ID 'productAudio'
+const productImage2 = document.getElementById('102'); // Selects the image element
+const productImage3 = document.getElementById('103'); // Selects the image element
+const productImage4 = document.getElementById('104'); // Selects the image element
+const productImage5 = document.getElementById('105'); // Selects the image element
+const productImage6 = document.getElementById('106'); // Selects the image element
+const productImage7 = document.getElementById('107'); // Selects the image element
+const productImage8 = document.getElementById('108'); // Selects the image element
+const productImage9 = document.getElementById('109'); // Selects the image element
+const productImage10 = document.getElementById('110'); // Selects the image element
+const productImage11 = document.getElementById('111'); // Selects the image element
+const productImage12 = document.getElementById('112'); // Selects the image element
+const productImage13 = document.getElementById('113'); // Selects the image element
+const productImage14 = document.getElementById('114'); // Selects the image element
+const productImage15 = document.getElementById('115'); // Selects the image element
+const productImage16 = document.getElementById('116'); // Selects the image element
+const productImage17 = document.getElementById('117'); // Selects the image element
+const productImage18 = document.getElementById('118'); // Selects the image element
+const productImage19 = document.getElementById('119'); // Selects the image element
+
+var audioEnReproduccion = new Audio('assets/audio/PDS70ID.mp3');
+productImage2.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/PDS70ID.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage3.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/HH46.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage4.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/SArc.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage5.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/RO.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage6.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/RN.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage7.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/E55CE.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage8.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/W2.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage9.addEventListener('click', () => {
+    audioEnReproduccion.pause();   
+    audioEnReproduccion = new Audio('assets/audio/C29P.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage10.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/PD.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage11.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/IRE.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage12.addEventListener('click', () => {
+    console.log("entre al 12");
+    console.log(audioEnReproduccion);
+    audioEnReproduccion.pause();
+
+    audioEnReproduccion = new Audio('assets/audio/GFEU.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage13.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/Me92.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage14.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/NGC2835.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage15.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/NGC604.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage16.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/M82.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage17.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/PDS70.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage18.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/M92.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
+
+productImage19.addEventListener('click', () => {
+    audioEnReproduccion.pause();
+    audioEnReproduccion = new Audio('assets/audio/Arp107.mp3');
+    audioEnReproduccion.play();
+  //productAudio.play(); // Play the audio when image is clicked
+});
